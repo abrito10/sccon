5e9ce49a02a28d41a3b998b08534ba1c49128386
adilson de brito - mac <abrito10@gmail.com>
2021-02-09 18:48:55 -0300

    projeto iniciado

---
+++ backend/src/main/java/com/abrito10/testesccon/TesteScconApplication.java
@@ -0,0 +1,13 @@
+package com.abrito10.testesccon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteScconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteScconApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/abrito10/testesccon/TesteScconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrito10.testesccon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteScconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
